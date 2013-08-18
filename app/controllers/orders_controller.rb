1d5eba0a2208cd74b41a4e5c3c2751a403f7f0ea
Tatyana <tatyana.kudiyarova@gmail.com>
2013-08-18 23:32:46 +1200

    Orders Controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :js
+
   def index
   	@orders = Order.all
   end
@@ -6,4 +9,32 @@ class OrdersController < ApplicationController
   def new
   	@order = Order.new
   end
+
+  def create
+  	@order = Order.new order_params
+  	if @order.save
+  	  redirect_to @order
+  	else
+  	  render action: 'new'
+  	end
+  end
+
+  def update
+  	@order.update_attributes! order_params
+  	redirect_to @order
+  end
+
+  def destroy
+  	@order.destroy
+  	redirect_to orders_url
+  end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order).permit(:name, :comment, :user_id, :drink_id)
+    end
 end
